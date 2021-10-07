f009f9cb060faa267368160415b42b3e034c5c5e
aryogiaziz <60728364+aryogiaziz@users.noreply.github.com>
2021-10-07 11:26:30 +0700

    Tugas Praktikum web laravel - video 4

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('container')
+
+<article>
+    <h2>{{ $post["title"] }}</h2>
+    <h5>{{ $post["author"] }}</h5>
+    <p>{{ $post["body"] }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia vel expedita, nisi obcaecati velit asperiores necessitatibus repellendus voluptates ratione eaque quisquam totam quia, harum sunt sint dolores modi ab. Unde, itaque totam cumque tempora exercitationem cupiditate minima voluptatem nisi natus ut quibusdam? Consectetur odio rem, suscipit praesentium quisquam iure in, ipsa recusandae provident eveniet ad? Eaque vero itaque magnam eos minima enim, quas modi esse dolore corporis maxime eum nihil architecto rem ut animi molestias ducimus necessitatibus quibusdam doloribus deleniti quos aspernatur veniam quia. Ut incidunt provident corrupti tenetur, atque rem fugiat! Iure excepturi, quasi temporibus consequuntur veniam rem tempora. Fuga, facere inventore eaque repellendus maxime animi. Consequatur facilis debitis quibusdam perferendis provident ut. Quibusdam, voluptatum expedita consectetur ad, provident reprehenderit obcaecati neque magni harum iure illum tempora, earum quos praesentium delectus repellendus. Iste est nobis incidunt hic, ducimus aspernatur veniam quis dolorum, beatae, vitae commodi consequuntur sed mollitia amet rem omnis accusantium iure illum eveniet? Corporis, dolorem voluptate repellendus excepturi blanditiis dolorum vero modi. Iste ut architecto reiciendis animi magni rerum praesentium quidem, porro, modi inventore natus voluptas nihil?</p>
+</article>
+
+<a href="/blog">Back to Posts</a>
+@endsection
