51d0ddcba76b4304133d3a99db4e6dd98caca23d
aryogiaziz <60728364+aryogiaziz@users.noreply.github.com>
2021-10-13 21:35:02 +0700

    Tugas Praktikum Web Laravel (bagian 2) - Video 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,60 +31,5 @@ Route::get('/about', function () {
     ]);
 });
 
-
-
-
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Aryogi Aziz",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias minima recusandae aliquam neque alias porro sapiente? Ipsum eligendi eum aspernatur fugiat ea explicabo esse fuga similique dolorem. Voluptatum in eveniet at exercitationem laborum quis, possimus, ullam sequi eius eligendi totam hic, praesentium magni. Distinctio consectetur esse fugit odit iusto, molestiae possimus perferendis, totam repellat eos temporibus eaque. Illum optio nihil quis tenetur nobis vitae doloremque. Adipisci laboriosam nisi, dignissimos in delectus modi, harum eos aliquam, suscipit veniam accusantium. Quisquam accusamus, cupiditate iure error doloremque repellendus ratione? Tempora, consequatur est?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Agung Gumelar",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias minima recusandae aliquam neque alias porro sapiente? Ipsum eligendi eum aspernatur fugiat ea explicabo esse fuga similique dolorem. Voluptatum in eveniet at exercitationem laborum quis, possimus, ullam sequi eius eligendi totam hic, praesentium magni. Distinctio consectetur esse fugit odit iusto, molestiae possimus perferendis, totam repellat eos temporibus eaque. Illum optio nihil quis tenetur nobis vitae doloremque. Adipisci laboriosam nisi, dignissimos in delectus modi, harum eos aliquam, suscipit veniam accusantium. Quisquam accusamus, cupiditate iure error doloremque repellendus ratione? Tempora, consequatur est?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam inventore repudiandae voluptatem expedita quo, veritatis eaque. Totam atque facilis esse, id laboriosam doloribus excepturi! Iure blanditiis, dignissimos nesciunt ipsam impedit sequi id at? Temporibus dignissimos nam non ducimus ex ad facilis? Et, pariatur est, mollitia vero eos architecto maxime nesciunt a nulla, eaque voluptas dignissimos amet! Vel libero et ipsam quos voluptates hic itaque unde soluta, voluptatibus facere illo! Illum repudiandae eveniet vero tenetur, amet quod eligendi qui tempore illo, recusandae ex reiciendis debitis hic temporibus quam? Ut saepe pariatur veritatis. Fugiat doloremque optio, vitae officia asperiores amet nulla placeat."
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-
-// halaman single post
-
-Route::get('posts/{slug}', function($slug){
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Aryogi Aziz",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias minima recusandae aliquam neque alias porro sapiente? Ipsum eligendi eum aspernatur fugiat ea explicabo esse fuga similique dolorem. Voluptatum in eveniet at exercitationem laborum quis, possimus, ullam sequi eius eligendi totam hic, praesentium magni. Distinctio consectetur esse fugit odit iusto, molestiae possimus perferendis, totam repellat eos temporibus eaque. Illum optio nihil quis tenetur nobis vitae doloremque. Adipisci laboriosam nisi, dignissimos in delectus modi, harum eos aliquam, suscipit veniam accusantium. Quisquam accusamus, cupiditate iure error doloremque repellendus ratione? Tempora, consequatur est?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Agung Gumelar",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias minima recusandae aliquam neque alias porro sapiente? Ipsum eligendi eum aspernatur fugiat ea explicabo esse fuga similique dolorem. Voluptatum in eveniet at exercitationem laborum quis, possimus, ullam sequi eius eligendi totam hic, praesentium magni. Distinctio consectetur esse fugit odit iusto, molestiae possimus perferendis, totam repellat eos temporibus eaque. Illum optio nihil quis tenetur nobis vitae doloremque. Adipisci laboriosam nisi, dignissimos in delectus modi, harum eos aliquam, suscipit veniam accusantium. Quisquam accusamus, cupiditate iure error doloremque repellendus ratione? Tempora, consequatur est?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam inventore repudiandae voluptatem expedita quo, veritatis eaque. Totam atque facilis esse, id laboriosam doloribus excepturi! Iure blanditiis, dignissimos nesciunt ipsam impedit sequi id at? Temporibus dignissimos nam non ducimus ex ad facilis? Et, pariatur est, mollitia vero eos architecto maxime nesciunt a nulla, eaque voluptas dignissimos amet! Vel libero et ipsam quos voluptates hic itaque unde soluta, voluptatibus facere illo! Illum repudiandae eveniet vero tenetur, amet quod eligendi qui tempore illo, recusandae ex reiciendis debitis hic temporibus quam? Ut saepe pariatur veritatis. Fugiat doloremque optio, vitae officia asperiores amet nulla placeat."
-        ]
-    ];
-
-
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
