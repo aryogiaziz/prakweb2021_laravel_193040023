4399f321d3b512cc83662bb38d235ccefa4bbe89
aryogiaziz <60728364+aryogiaziz@users.noreply.github.com>
2021-10-14 09:07:16 +0700

    Tugas Praktikum Web Laravel (bagian 2) - Video 4

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
 
 
     <h2 class="mb-5">{{ $post->title }}</h2>
-    <h5>{{ $post->author }}</h5>
+    <p>By. Aryogi Aziz in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,13 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('home', [
         "title" => "Home"
     ]);
 });
 
-Route::get('/about', function () {
+Route::get('/about', function() {
     return view('about', [
         "title" => "About",
         "name" => "Aryogi Aziz",
@@ -33,3 +34,18 @@ Route::get('/about', function () {
 
 Route::get('/blog', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category){
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
