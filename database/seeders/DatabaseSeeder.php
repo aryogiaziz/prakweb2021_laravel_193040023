fa8861b5a4c598bf3d91ace9ce237e34801751a2
aryogiaziz <60728364+aryogiaziz@users.noreply.github.com>
2021-10-21 07:45:24 +0700

    Tugas Praktikum Web Laravel (Bagian 3) - Video 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,64 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Aryogi Aziz',
+            'email' => 'aaryogi@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Agung',
+            'email' => 'agung@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, cum, libero amet rerum cumque dolorem corporis expedita quibusdam, accusamus vitae debitis iusto officia repudiandae deleniti tempora ipsa sed quod?',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, cum, libero amet rerum cumque dolorem corporis expedita quibusdam, accusamus vitae debitis iusto officia repudiandae deleniti tempora ipsa sed quod? Ea, voluptate quis assumenda pariatur eveniet qui aspernatur in, laboriosam rem reiciendis obcaecati magni, fuga hic animi eligendi quas corrupti labore consequatur tempora unde excepturi itaque sunt modi! Consectetur a repellendus illo! Ut labore illo at minima esse ducimus voluptatibus officia harum, natus placeat temporibus eaque ipsam distinctio unde soluta, rem perspiciatis nulla itaque rerum, pariatur amet sequi ad cumque neque? Recusandae sapiente illo laboriosam ut voluptates officiis quo commodi accusamus!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, cum, libero amet rerum cumque dolorem corporis expedita quibusdam, accusamus vitae debitis iusto officia repudiandae deleniti tempora ipsa sed quod?',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, cum, libero amet rerum cumque dolorem corporis expedita quibusdam, accusamus vitae debitis iusto officia repudiandae deleniti tempora ipsa sed quod? Ea, voluptate quis assumenda pariatur eveniet qui aspernatur in, laboriosam rem reiciendis obcaecati magni, fuga hic animi eligendi quas corrupti labore consequatur tempora unde excepturi itaque sunt modi! Consectetur a repellendus illo! Ut labore illo at minima esse ducimus voluptatibus officia harum, natus placeat temporibus eaque ipsam distinctio unde soluta, rem perspiciatis nulla itaque rerum, pariatur amet sequi ad cumque neque? Recusandae sapiente illo laboriosam ut voluptates officiis quo commodi accusamus!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, cum, libero amet rerum cumque dolorem corporis expedita quibusdam, accusamus vitae debitis iusto officia repudiandae deleniti tempora ipsa sed quod?',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, cum, libero amet rerum cumque dolorem corporis expedita quibusdam, accusamus vitae debitis iusto officia repudiandae deleniti tempora ipsa sed quod? Ea, voluptate quis assumenda pariatur eveniet qui aspernatur in, laboriosam rem reiciendis obcaecati magni, fuga hic animi eligendi quas corrupti labore consequatur tempora unde excepturi itaque sunt modi! Consectetur a repellendus illo! Ut labore illo at minima esse ducimus voluptatibus officia harum, natus placeat temporibus eaque ipsam distinctio unde soluta, rem perspiciatis nulla itaque rerum, pariatur amet sequi ad cumque neque? Recusandae sapiente illo laboriosam ut voluptates officiis quo commodi accusamus!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, cum, libero amet rerum cumque dolorem corporis expedita quibusdam, accusamus vitae debitis iusto officia repudiandae deleniti tempora ipsa sed quod?',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, cum, libero amet rerum cumque dolorem corporis expedita quibusdam, accusamus vitae debitis iusto officia repudiandae deleniti tempora ipsa sed quod? Ea, voluptate quis assumenda pariatur eveniet qui aspernatur in, laboriosam rem reiciendis obcaecati magni, fuga hic animi eligendi quas corrupti labore consequatur tempora unde excepturi itaque sunt modi! Consectetur a repellendus illo! Ut labore illo at minima esse ducimus voluptatibus officia harum, natus placeat temporibus eaque ipsam distinctio unde soluta, rem perspiciatis nulla itaque rerum, pariatur amet sequi ad cumque neque? Recusandae sapiente illo laboriosam ut voluptates officiis quo commodi accusamus!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
     }
 }
